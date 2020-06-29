8e2dc1422ce0bc094b8f22abbc3939ebc1c64afc
Winston Nwanne <wnwanne@gmail.com>
2020-06-29 18:11:26 -0400

    removed reduntdant conversions between PIL and cv2 and included functionality for ball tracing

--- recognize_image.py
+++ recognize_image.py
@@ -18,10 +18,10 @@ def show_custom_labels(model, photo, min_confidence):
     # stream = io.BytesIO(s3_response['Body'].read())
 
     # read in photo
-    image = cv2.imread(photo)
+    frame = cv2.imread(photo)
 
     # if has frame then resize and encode it as jpg
-    hasFrame, imageBytes = cv2.imencode(".jpg", image)
+    hasFrame, imageBytes = cv2.imencode(".jpg", frame)
 
     # run image against model
     response = rekognition.detect_custom_labels(
@@ -31,11 +31,13 @@ def show_custom_labels(model, photo, min_confidence):
         ProjectVersionArn=model,
         MinConfidence=min_confidence)
 
-    #convert from cv2 to PIL
-    image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    # #convert from cv2 to PIL
+    # image_pil = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    imgWidth, imgHeight  = image_pil.size
-    draw = ImageDraw.Draw(image_pil)
+    # imgWidth, imgHeight  = image_pil.size
+    # draw = ImageDraw.Draw(image_pil)
+
+    imgHeight, imgWidth, imgChannel = frame.shape
 
     # calculate and display bounding boxes for each detected custom label
     print('Detected custom labels for ' + photo)
@@ -51,8 +53,8 @@ def show_custom_labels(model, photo, min_confidence):
             x = left + (width / 2)
             y = top - (height / 2)
 
-            fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 50)
-            draw.text((left, top), customLabel['Name'], fill='#00d400', font=fnt)
+            # fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 50)
+            # draw.text((left, top), customLabel['Name'], fill='#00d400', font=fnt)
 
             print('Left: ' + '{0:.0f}'.format(left))
             print('Top: ' + '{0:.0f}'.format(top))
@@ -64,12 +66,19 @@ def show_custom_labels(model, photo, min_confidence):
                 (left, top),
                 (left + width, top),
                 (left + width, top + height),
-                (left, top + height),
                 (left, top))
-            draw.line(points, fill='#00d400', width=5)
 
-    image_pil.show()
+            cv2.line(frame, (int(left), int(top)), (int(left) + int(width), int(top)), color=(0, 0, 0))
+            cv2.line(frame, (int(left) + int(width), int(top)), (int(left) + int(width), int(top) + int(height)), color=(0, 0, 0))
+            cv2.line(frame, (int(left) + int(width), int(top) + int(height)), (int(left), int(top) + int(width)), color=(0, 0, 0))
+            cv2.line(frame, (int(left), int(top) + int(width)), (int(left), int(top)), color=(0, 0, 0))
 
+            # draw.line(points, fill='#00d400', width=5)
+            print(points)
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
     return len(response['CustomLabels'])
 
 
--- rekognize_video.py
+++ rekognize_video.py
@@ -1,11 +1,8 @@
-import json
+from _collections import deque
 import boto3
 import numpy as np
 import cv2
-import math
-# from sort import *
-import io
-import time
+from datetime import datetime
 from PIL import Image, ImageDraw, ExifTags, ImageColor, ImageFont
 
 model = 'arn:aws:rekognition:us-east-1:333527701433:project/winnie_test_training/version/' \
@@ -18,12 +15,14 @@ def analyzeVideo(video, model, min_confidence):
     fps = vid.get(cv2.CAP_PROP_FPS)  # frame rate
     frame_width = int(vid.get(3))
     frame_height = int(vid.get(4))
+    pts = deque()
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-    current_time = time.time()
 
-    # Define the codec and create VideoWriter object.The output is stored in 'outpy.avi' file.
+    # Define the codec and create VideoWriter object.The output is stored in 'outputs-"date and time".mp4' file.
     # Define the fps to be equal to 10. Also frame size is passed.
-    out = cv2.VideoWriter('Demo Media/Outputs/outputs-{}.mp4'.format(current_time), cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter('Demo Media/Outputs/outputs-{}.avi'.format(now), fourcc, 10, (frame_width, frame_height))
 
     while (vid.isOpened()):
         frameId = vid.get(1)  # current frame number
@@ -52,49 +51,78 @@ def analyzeVideo(video, model, min_confidence):
                 MinConfidence=min_confidence
             )
 
-            # convert from cv2 to PIL
-            image_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            # # convert from cv2 to PIL
+            # image_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
-            imgWidth, imgHeight = image_pil.size
-            draw = ImageDraw.Draw(image_pil)
+            imgHeight, imgWidth, imgChannel  = frame.shape
+            # draw = ImageDraw.Draw(image_pil)
 
             # calculate and display bounding boxes for each detected custom label
             for customLabel in response['CustomLabels']:
                 # print('Label ' + str(customLabel['Name']))
                 # print('Confidence ' + str(customLabel['Confidence']))
-                if 'Geometry' in customLabel:
-                    box = customLabel['Geometry']['BoundingBox']
-                    left = imgWidth * box['Left']
-                    top = imgHeight * box['Top']
-                    width = imgWidth * box['Width']
-                    height = imgHeight * box['Height']
-                    x = left + (width/2)
-                    y = top - (height/2)
+                if customLabel['Name'] == 'ball':
+                    if 'Geometry' in customLabel:
+                        box = customLabel['Geometry']['BoundingBox']
+                        left = imgWidth * box['Left']
+                        top = imgHeight * box['Top']
+                        width = imgWidth * box['Width']
+                        height = imgHeight * box['Height']
+                        x = left + (width/2)
+                        y = top - (height/2)
+                        coords = (int(x), int(y))
+                        # update the points queue
+                        pts.appendleft(coords)
 
-                    fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 50)
-                    draw.text((left, top), customLabel['Name'], fill='#00d400', font=fnt)
-                    draw.point((x,y), fill=255)
+                        # fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 50)
+                        # draw.text((left, top), customLabel['Name'], fill='#00d400', font=fnt)
+                        # draw.point((x,y), fill=255)
 
-                    print('label')
-                    print('Left: ' + '{0:.0f}'.format(left))
-                    print('Top: ' + '{0:.0f}'.format(top))
-                    print('Label Width: ' + "{0:.0f}".format(width))
-                    print('Label Height: ' + "{0:.0f}".format(height))
-                    print('x coor: {}, y coor: {}'.format(x,y))
+                        print('label')
+                        print('Left: ' + '{0:.0f}'.format(left))
+                        print('Top: ' + '{0:.0f}'.format(top))
+                        print('Label Width: ' + "{0:.0f}".format(width))
+                        print('Label Height: ' + "{0:.0f}".format(height))
+                        print('x coor: {}, y coor: {}'.format(x,y))
+                        print(pts)
 
-                    points = (
-                        (left, top),
-                        (left + width, top),
-                        (left + width, top + height),
-                        (left, top + height),
-                        (left, top))
-                    draw.line(points, fill='#00d400', width=5)
+                        points = (
+                            (left, top),
+                            (left + width, top),
+                            (left + width, top + height),
+                            (left, top + height),
+                            (left, top))
 
-        numpy_image = np.array(image_pil)
-        opencv_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+                        # draw.line(points, fill='#00d400', width=5)
+                        cv2.line(frame, (int(left), int(top)), (int(left) + int(width), int(top)), color=(0, 0, 0), thickness=3)
+                        cv2.line(frame, (int(left) + int(width), int(top)),
+                                 (int(left) + int(width), int(top) + int(height)), color=(0, 0, 0), thickness=3)
+                        cv2.line(frame, (int(left) + int(width), int(top) + int(height)),
+                                 (int(left), int(top) + int(width)), color=(0, 0, 0), thickness=3)
+                        cv2.line(frame, (int(left), int(top) + int(width)), (int(left), int(top)), color=(0, 0, 0), thickness=3)
 
-        out.write(opencv_image)
-        cv2.imshow('frame', opencv_image)
+
+
+        # numpy_image = np.array(image_pil)
+        # opencv_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+
+
+
+
+
+        # loop over the set of tracked points
+        for i in range(1, len(pts)):
+            # if either of the tracked points are None, ignore them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
+
+            # otherwise, compute the thickness of the line and draw the connecting lines
+            thickness = 2
+            cv2.line(frame, pts[i - 1], pts[i], (44, 255, 20), thickness)
+
+        out.write(frame)
+        cv2.imshow('frame', frame)
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -102,10 +130,10 @@ def analyzeVideo(video, model, min_confidence):
     out.release()
 
 def main():
-    video = "Demo Media/winnie_shooting.mov"
+    video = "Demo Media/winnie_shooting2.mov"
     model = 'arn:aws:rekognition:us-east-1:333527701433:project/winnie_test_training/version/' \
             'winnie_test_training.2020-04-30T22.35.42/1588300542347'
-    min_confidence = 99
+    min_confidence = 98
 
     analyzeVideo(video, model, min_confidence)
 
